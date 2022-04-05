2211167a625f99b6902696e74f9fafd45b8252e8
danineon <danielgg944@gmail.com>
2022-04-05 15:12:48 +0200

    Creacion de piezas automatizada

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -17,6 +17,15 @@ export class HomePage implements OnInit {
 
 function trocearImagen() {
 
+  var startX, startY
+  var areaX, areaY
+  var posX, posY
+  var width, height
+  var cont1, cont2
+  var i, j
+  var rows, columns
+  const scale = 100
+
   var image = new Image(),
     canvas = <HTMLCanvasElement>document.getElementById('canvas')
   var ctx = canvas.getContext('2d')
@@ -24,6 +33,52 @@ function trocearImagen() {
   image.src = '../assets/imgs/pikachu.jpg'
 
   image.onload = function () {
+
+    height = image.height
+    width = image.width
+    startX = 0
+    startY = 0
+    posX = 0
+    posY = 0
+    rows = 3
+    columns = 3
+    areaX = width / rows
+    areaY = height / columns
+    cont1 = 0
+    cont2 = 0
+
+    for (i = 0; i < columns; i++) {
+      if (cont1 != 0) {
+        startY = startY + areaY
+        posY = posY + scale + 1
+        cont1++
+      }
+      else {
+        cont1++
+      }
+      for (j = 0; j < rows; j++) {
+        if (cont2 == 3) {
+          cont2 = 0
+          startX = 0
+          posX = 0
+        }
+        if (cont2 != 0) {
+          startX = startX + areaX
+          posX = posX + scale + 1
+          cont2++
+        }
+        else {
+          cont2++
+        }
+        ctx.drawImage(image,
+          startX, startY,   // Start at 70/20 pixels from the left and the top of the image (crop),
+          areaX, areaY,   // "Get" a `50 * 50` (w * h) area from the source image (crop),
+          posX, posY,     // Place the result at 0, 0 in the canvas,
+          scale, scale); // With as width / height: 100 * 100 (scale)
+
+      }
+    }
+    /*
     ctx.drawImage(image,
       0, 0,   // Start at 70/20 pixels from the left and the top of the image (crop),
       400, 300,   // "Get" a `50 * 50` (w * h) area from the source image (crop),
@@ -77,7 +132,7 @@ function trocearImagen() {
       400, 300,   // "Get" a `50 * 50` (w * h) area from the source image (crop),
       202, 202,     // Place the result at 0, 0 in the canvas,
       100, 100); // With as width / height: 100 * 100 (scale)
-
+     */
 
   }
 }
