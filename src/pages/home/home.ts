932c6d91f66bb0eb755099fc4ca2f8ba5d85951b
danineon <danielgg944@gmail.com>
2022-05-18 18:49:53 +0200

    Añadido forma de piezas de puzle y cambio a seleccion por color

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -44,6 +44,8 @@ export class HomePage implements OnInit {
 let CANVAS = null
 let CONTEXT = null
 let IMAGE = new Image()
+let HELPER_CANVAS = null
+let HELPER_CONTEXT = null
 let SCALER = 0.6
 let SIZE = { x: 0, y: 0, width: 0, height: 0, rows: 3, columns: 3 }
 let PIECES = []
@@ -53,15 +55,19 @@ let END_TIME = null
 let TIME = null
 
 let POP_SOUND = new Audio("../assets/sounds/pop.mp3")
-POP_SOUND.volume=0.5
+POP_SOUND.volume = 0.5
 
 let COMPLETE_SOUND = new Audio("../assets/sounds/complete.mp3")
-COMPLETE_SOUND.volume=0.2
+COMPLETE_SOUND.volume = 0.2
 
 function main() {
 
   CANVAS = document.getElementById('canvas')
   CONTEXT = CANVAS.getContext("2d")
+
+  HELPER_CANVAS = document.getElementById('helperCanvas')
+  HELPER_CONTEXT = HELPER_CANVAS.getContext("2d")
+
   addEventListeners()
 
   IMAGE.src = '../assets/imgs/pikachu.jpg'
@@ -69,8 +75,7 @@ function main() {
   IMAGE.onload = function () {
     handleResize()
     window.addEventListener('resize', handleResize)
-    initializePieces(3, 3)
-    //randomizePieces()
+    initializePieces(SIZE.rows, SIZE.columns)
     updateGame()
   }
 }
@@ -78,11 +83,11 @@ function main() {
 function updateTime() {
   let now = new Date().getTime()
   if (START_TIME != null) {
-    if(END_TIME!=null){
+    if (END_TIME != null) {
       TIME = document.getElementById("time")
       TIME.innerHTML = formatTime(END_TIME - START_TIME)
     }
-    else{
+    else {
       TIME = document.getElementById("time")
       TIME.innerHTML = formatTime(now - START_TIME)
     }
@@ -91,9 +96,9 @@ function updateTime() {
   }
 }
 
-function isComplete(){
-  for(let i=0; i<PIECES.length; i++){
-    if(PIECES[i].correct==false){
+function isComplete() {
+  for (let i = 0; i < PIECES.length; i++) {
+    if (PIECES[i].correct == false) {
       return false
     }
   }
@@ -145,7 +150,13 @@ function onTouchEnd() {
 }
 
 function onMouseDown(evt) {
-  SELECTED_PIECE = getPressedPiece(evt)
+  const imgData = HELPER_CONTEXT.getImageData(evt.x, evt.y, 1, 1)
+  if (imgData.data[3] == 0) {
+    return
+  }
+  const clickedColor = "rgb(" + imgData.data[0] + "," + imgData.data[1] + "," + imgData.data[2] + ")"
+  SELECTED_PIECE = getPressedPieceByColor(evt, clickedColor)
+  //SELECTED_PIECE = getPressedPiece(evt)
   if (SELECTED_PIECE != null) {
     const index = PIECES.indexOf(SELECTED_PIECE)
     if (index > -1) {
@@ -172,7 +183,7 @@ function onMouseUp() {
   if (SELECTED_PIECE != null) {
     if (SELECTED_PIECE.isClose()) {
       SELECTED_PIECE.snap()
-      if(isComplete() && END_TIME == null){
+      if (isComplete() && END_TIME == null) {
         let now = new Date().getTime()
         END_TIME = now
         COMPLETE_SOUND.play()
@@ -184,7 +195,6 @@ function onMouseUp() {
 }
 
 function getPressedPiece(loc) {
-  console.log('MOUSE => X: ' + loc.x + '  Y: ' + loc.y);
 
   for (let i = PIECES.length - 1; i >= 0; i--) {
     if (loc.x > PIECES[i].x && loc.x < PIECES[i].x + PIECES[i].width &&
@@ -197,9 +207,25 @@ function getPressedPiece(loc) {
   return null
 }
 
+function getPressedPieceByColor(loc, color) {
+  //console.log('MOUSE => X: ' + loc.x + '  Y: ' + loc.y);
+
+  for (let i = PIECES.length - 1; i >= 0; i--) {
+    if (PIECES[i].color == color) {
+      console.log("ENTRA");
+      
+      return PIECES[i]
+    }
+  }
+  return null
+}
+
 function handleResize() {
   CANVAS.width = window.innerWidth
   CANVAS.height = window.innerHeight
+
+  HELPER_CANVAS.width = window.innerWidth
+  HELPER_CANVAS.height = window.innerHeight
 
   let resizer = SCALER * Math.min(
     window.innerWidth / IMAGE.width,
@@ -215,6 +241,7 @@ function handleResize() {
 
 function updateGame() {
   CONTEXT.clearRect(0, 0, CANVAS.width, CANVAS.height)
+  HELPER_CONTEXT.clearRect(0, 0, CANVAS.width, CANVAS.height)
 
   CONTEXT.globalAlpha = 0.5
   CONTEXT.drawImage(IMAGE,
@@ -224,10 +251,19 @@ function updateGame() {
 
   for (let i = 0; i < PIECES.length; i++) {
     PIECES[i].draw(CONTEXT)
+    PIECES[i].draw(HELPER_CONTEXT, false)
   }
 
   updateTime()
   window.requestAnimationFrame(updateGame)
+}
+
+function getRandomColor() {
+  const red = Math.floor(Math.random() * 255)
+  const green = Math.floor(Math.random() * 255)
+  const blue = Math.floor(Math.random() * 255)
+
+  return "rgb(" + red + "," + green + "," + blue + ")"
 }
 
 function initializePieces(rows, cols) {
@@ -235,11 +271,54 @@ function initializePieces(rows, cols) {
   SIZE.columns = cols
 
   PIECES = []
+  const uniqueRandomColors = []
   for (let i = 0; i < SIZE.rows; i++) {
     for (let j = 0; j < SIZE.columns; j++) {
-      PIECES.push(new Piece(i, j))
+      let color = getRandomColor()
+      while (uniqueRandomColors.includes(color)) {
+        color = getRandomColor()
+      }
+      PIECES.push(new Piece(i, j, color))
     }
   }
+
+  let cnt = 0
+  for (let i = 0; i < SIZE.rows; i++) {
+    for (let j = 0; j < SIZE.columns; j++) {
+      const piece = PIECES[cnt]
+      if (i == SIZE.rows - 1) {
+        piece.bottom = null
+      }
+      else {
+        const sgn = (Math.random() - 0.5) < 0 ? -1 : 1
+        piece.bottom = sgn * (Math.random() * 0.4 + 0.3)
+      }
+
+      if (j == SIZE.columns - 1) {
+        piece.right = null
+      }
+      else {
+        const sgn = (Math.random() - 0.5) < 0 ? -1 : 1
+        piece.right = sgn * (Math.random() * 0.4 + 0.3)
+      }
+
+      if (j == 0) {
+        piece.left = null
+      }
+      else {
+        piece.left = -PIECES[cnt - 1].right
+      }
+
+      if (i == 0) {
+        piece.top = null
+      }
+      else {
+        piece.top = -PIECES[cnt - SIZE.columns].bottom
+      }
+      cnt++
+    }
+  }
+
 }
 
 function randomizePieces() {
@@ -263,8 +342,13 @@ class Piece {
   xCorrect: number
   yCorrect: number
   correct: boolean
+  top: number
+  right: number
+  left: number
+  bottom: number
+  color: any
 
-  constructor(rowIndex, colIndex) {
+  constructor(rowIndex, colIndex, color) {
     this.rowIndex = rowIndex
     this.colIndex = colIndex
     this.x = SIZE.x + SIZE.width * this.colIndex / SIZE.columns
@@ -274,21 +358,154 @@ class Piece {
     this.xCorrect = this.x
     this.yCorrect = this.y
     this.correct = true
+    this.color = color
   }
-  draw(context: any) {
+  draw(context: any, useImage = true) {
     context.beginPath()
 
-    context.drawImage(IMAGE,
-      this.colIndex * IMAGE.width / SIZE.columns,
-      this.rowIndex * IMAGE.height / SIZE.rows,
-      IMAGE.width / SIZE.columns,
-      IMAGE.height / SIZE.rows,
-      this.x,
-      this.y,
-      this.width,
-      this.height)
+    const sz = Math.min(this.width, this.height)
+    const neck = 0.1 * sz
+    const tabWidth = 0.2 * sz
+    const tabHeight = 0.2 * sz
 
-    context.rect(this.x, this.y, this.width, this.height)
+    //context.rect(this.x, this.y, this.width, this.height)
+    //from top left
+    context.moveTo(this.x, this.y)
+    //to top right
+    if (this.top) {
+      context.lineTo(this.x + this.width * Math.abs(this.top) - neck, this.y)
+
+      context.bezierCurveTo(
+        this.x + this.width * Math.abs(this.top) - neck,
+        this.y - tabHeight * Math.sign(this.top) * 0.2,
+
+        this.x + this.width * Math.abs(this.top) - tabWidth,
+        this.y - tabHeight * Math.sign(this.top),
+
+        this.x + this.width * Math.abs(this.top),
+        this.y - tabHeight * Math.sign(this.top)
+      )
+
+      context.bezierCurveTo(
+        this.x + this.width * Math.abs(this.top) + tabWidth,
+        this.y - tabHeight * Math.sign(this.top),
+
+        this.x + this.width * Math.abs(this.top) + neck,
+        this.y - tabHeight * Math.sign(this.top) * 0.2,
+
+        this.x + this.width * Math.abs(this.top) + neck, this.y
+      )
+    }
+    context.lineTo(this.x + this.width, this.y)
+    //to bottom right
+    if (this.right) {
+      context.lineTo(this.x + this.width, this.y + this.height * Math.abs(this.right) - neck)
+
+      context.bezierCurveTo(
+        this.x + this.width - tabHeight * Math.sign(this.right) * 0.2,
+        this.y + this.height * Math.abs(this.right) - neck,
+
+        this.x + this.width - tabHeight * Math.sign(this.right),
+        this.y + this.height * Math.abs(this.right) - tabWidth,
+
+        this.x + this.width - tabHeight * Math.sign(this.right),
+        this.y + this.height * Math.abs(this.right)
+      )
+
+      context.bezierCurveTo(
+        this.x + this.width - tabHeight * Math.sign(this.right),
+        this.y + this.height * Math.abs(this.right) + tabWidth,
+
+        this.x + this.width - tabHeight * Math.sign(this.right) * 0.2,
+        this.y + this.height * Math.abs(this.right) + neck,
+
+        this.x + this.width,
+        this.y + this.height * Math.abs(this.right) + neck
+      )
+
+    }
+    context.lineTo(this.x + this.width, this.y + this.height)
+    //to bottom left
+    if (this.bottom) {
+      context.lineTo(this.x + this.width * Math.abs(this.bottom) + neck, this.y + this.height)
+
+      context.bezierCurveTo(
+        this.x + this.width * Math.abs(this.bottom) + neck,
+        this.y + this.height + tabHeight * Math.sign(this.bottom) * 0.2,
+
+        this.x + this.width * Math.abs(this.bottom) + tabWidth,
+        this.y + this.height + tabHeight * Math.sign(this.bottom),
+
+        this.x + this.width * Math.abs(this.bottom),
+        this.y + this.height + tabHeight * Math.sign(this.bottom)
+      )
+
+      context.bezierCurveTo(
+        this.x + this.width * Math.abs(this.bottom) - tabWidth,
+        this.y + this.height + tabHeight * Math.sign(this.bottom),
+
+        this.x + this.width * Math.abs(this.bottom) - neck,
+        this.y + this.height + tabHeight * Math.sign(this.bottom) * 0.2,
+
+        this.x + this.width * Math.abs(this.bottom) - neck,
+        this.y + this.height
+      )
+    }
+    context.lineTo(this.x, this.y + this.height)
+    //to top left
+    if (this.left) {
+      context.lineTo(this.x, this.y + this.height * Math.abs(this.left) + neck)
+
+      context.bezierCurveTo(
+        this.x + tabHeight * Math.sign(this.left) * 0.2,
+        this.y + this.height * Math.abs(this.left) + neck,
+
+        this.x + tabHeight * Math.sign(this.left),
+        this.y + this.height * Math.abs(this.left) + tabWidth,
+
+        this.x + tabHeight * Math.sign(this.left),
+        this.y + this.height * Math.abs(this.left)
+      )
+
+      context.bezierCurveTo(
+        this.x + tabHeight * Math.sign(this.left),
+        this.y + this.height * Math.abs(this.left) - tabWidth,
+
+        this.x + tabHeight * Math.sign(this.left) * 0.2,
+        this.y + this.height * Math.abs(this.left) - neck,
+
+        this.x,
+        this.y + this.height * Math.abs(this.left) - neck
+      )
+    }
+
+    context.lineTo(this.x, this.y)
+
+    context.save()
+    context.clip()
+
+    const scaledTabHeight =
+      Math.min(IMAGE.width / SIZE.columns,
+        IMAGE.height / SIZE.rows) * tabHeight / sz
+
+    if (useImage) {
+      context.drawImage(IMAGE,
+        this.colIndex * IMAGE.width / SIZE.columns - scaledTabHeight,
+        this.rowIndex * IMAGE.height / SIZE.rows - scaledTabHeight,
+        IMAGE.width / SIZE.columns + scaledTabHeight * 2,
+        IMAGE.height / SIZE.rows + scaledTabHeight * 2,
+        this.x - tabHeight,
+        this.y - tabHeight,
+        this.width + tabHeight * 2,
+        this.height + tabHeight * 2)
+    }
+    else {
+      context.fillStyle = this.color
+      context.fillRect(this.x - tabHeight, this.y - tabHeight, this.width + tabHeight * 2, this.height * tabHeight * 2)
+    }
+
+    context.restore()
+
     context.stroke()
   }
 
